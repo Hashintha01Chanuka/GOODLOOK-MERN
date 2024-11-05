b1093422a27bb8cd5dda600fa11eba0e86450bbc
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2024-11-05 10:42:22 +0530

    navbar component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,12 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './component/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/collection' element={<Collection/>}/>
@@ -22,9 +24,8 @@ const App = () => {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/place-order' element={<PlaceOrder/>}/>
-        <Route path='/orders' element={<Orders/>}/>
-      </Routes>
-      
+        <Route path='/orders' element={<Orders/>}/>     
+      </Routes> 
     </div>
   )
 }
