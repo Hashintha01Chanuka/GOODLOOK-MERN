58c35a3ab0bf0d570b2d3f8e8349907fbb1c45e9
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2024-11-13 21:47:22 +0530

    create latestCollection component

--- frontend/src/component/LatestCollection.jsx
+++ frontend/src/component/LatestCollection.jsx
@@ -13,7 +13,7 @@ const LatestCollection = () => {
  return (
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
-            <Title text1={'LATEST'}  text2={'COLLECTION'} />
+            <Title text1={"LATEST"}  text2={"COLLECTION"} />
 
         </div>
       
