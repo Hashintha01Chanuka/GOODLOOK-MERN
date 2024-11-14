1ff3a93806e8ee1ba53f36ca370a4b52bb7612a3
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2024-11-14 20:57:03 +0530

    create array to get 10 products

--- frontend/src/component/LatestCollection.jsx
+++ frontend/src/component/LatestCollection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
@@ -6,6 +6,11 @@ import Title from './Title';
 const LatestCollection = () => {
  
      const { products } = useContext(ShopContext);
+     const [latestProducts, setLatestProducts] = useState([]);
+
+     useEffect(()=>{
+       setLatestProducts(products.slice(0,10));
+     },[] )
  
      
      
@@ -14,7 +19,7 @@ const LatestCollection = () => {
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
             <Title text1={"LATEST"}  text2={"COLLECTION"} />
-
+            <p className='w-3/4 m-auto text-xs sm:text-base text-gray-600'>This is our new products</p>
         </div>
       
     </div>
