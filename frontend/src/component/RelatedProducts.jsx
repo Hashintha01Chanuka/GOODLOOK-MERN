d4c868ba43760d142dc0a585c159e5e4c53f700a
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2024-11-25 21:52:41 +0530

    modified the file

--- frontend/src/component/RelatedProducts.jsx
+++ frontend/src/component/RelatedProducts.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from '../component/Title'
+import ProductItem from '../component/ProductItem'
 
 const RelatedProducts = ({category,subCategory}) => {
 
@@ -12,9 +14,9 @@ const RelatedProducts = ({category,subCategory}) => {
             let productCopy = products.slice();
 
             productCopy = productCopy.filter((item)=> category === item.category);
-            productCopy = productCopy.filter((item)=> subCategoryategory === item.subCategory);
+            productCopy = productCopy.filter((item)=> subCategory === item.subCategory);
 
-            console.log(productCopy.slice(0,5));
+            setRelated(productCopy.slice(0,5));
             
 
         }
@@ -22,8 +24,21 @@ const RelatedProducts = ({category,subCategory}) => {
 
 
   return (
-    <div>
-      
+    <div className='my-24'>
+        <div className='text-center text-3xl py-2'>
+            <Title text1={'Related'} text2={'Products'}/>
+        </div>
+
+        <div className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+                related.map((item,index)=>(
+                    <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image}/>
+                ))
+
+
+            }
+        </div>
+
     </div>
   )
 }
