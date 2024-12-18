cb9c3ad21a2c5d301d52c3d3c9799778e679ba0c
Hashintha01Chanuka <hashinthachanuka01@gmail.com>
2024-12-18 07:08:52 +0530

    Add cart functions

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -40,6 +40,16 @@ const ShopContextProvider = (props) => {
         }
         setCartItems(cartData);
 
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/add',{itemId,size},{headers:{token}})
+            
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
+
     }
 
   
@@ -64,6 +74,16 @@ const ShopContextProvider = (props) => {
         let cartData = structuredClone(cartItems);
         cartData[itemId][size] = quantity;
         setCartItems(cartData);
+
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/update',{itemId,size,quantity},{headers:{token}})
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
+
     }
 
     const getCartAmount = ()=> {
@@ -101,14 +121,37 @@ const ShopContextProvider = (props) => {
 
     }
 
+    const getUserCart = async (token)=>{
+        try {
+
+            const response = await axios.post(backendUrl + '/api/cart/get',{},{headers:{token}})
+            if (response.data.success) {
+                setCartItems(response.data.cartData)
+            }
+            
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+
+
     useEffect(()=>{
         getProductsData()
+    },[])
+
+    useEffect(()=>{
+        if (!token && localStorage.getItem('token')) {
+           setToken(localStorage.getItem('token')) 
+           getUserCart(localStorage.getItem('token'))
+        }
     },[])
 
     const value = {
         products , currency , delivery_fee,
         search,setSearch,showSearch,setShowSearch,
-        cartItems,addToCart,
+        cartItems,addToCart,setCartItems,
         getCartCount,updateQuantity,
         getCartAmount, navigate , backendUrl,
         setToken, token
